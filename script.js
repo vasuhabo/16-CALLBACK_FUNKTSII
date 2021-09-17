08394aa054529be44725b60a0fb87b584e5bdfd7
sahumo <7523460@gmail.com>
2021-09-18 00:34:10 +0300

    callback  ФУНКЦИИ 2

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ FIRST();
 SECOND ();
  
 
+
+
 function LEARN_JS(lang,callback) {
 console.log(`Я УЧУ:  ${lang}`);
 callback();
@@ -24,11 +26,26 @@ callback();
 LEARN_JS('JavaScript', function(){
 console.log('Я ПРОШЕЛ ЭТОТ УРОК')
 });
+/* ВЫВОДИТ  ТАК  
+Я УЧУ:  JavaScript
+Я ПРОШЕЛ ЭТОТ УРОК */
 
 
 
+function LEARN_JS(lang,callback) {
+  console.log(`Я УЧУ:  ${lang}`);
+  callback();
+  }
+  
+  function DONE () {
+    console.log('Я ПРОШЕЛ ЭТОТ УРОК');
+  }
 
-
+  LEARN_JS('JavaScript', DONE );
+  /* ВЫВОДИТ  ТАК  
+Я УЧУ:  JavaScript
+Я ПРОШЕЛ ЭТОТ УРОК */
+  
 
 
 
